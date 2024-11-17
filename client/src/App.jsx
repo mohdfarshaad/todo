2563c2a9810c7e74ca7800db257bff144ed26384
Mohammed Farshad <imohammedfarshad@gmail.com>
2024-11-18 02:06:48 +0530

    add creating todo feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Input } from "./components/index.js";
-import { createNewTodo } from "./api/todoService.js";
+import { createNewTodo, getTodos } from "./api/todoService.js";
+import Toast from "./components/Toast.jsx";
 
 function App() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [toast, setToast] = useState({ message: "", type: "" });
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    try {
+      const respone = getTodos();
+      setTodo(respone.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [title, description]);
 
   const createTodo = async () => {
-    const response = await createNewTodo({ title, description });
-    if (!response) {
-      console.log("Failed to add todo");
+    if (!title.trim()) {
+      console.log("Title is required");
+
+      setToast({ message: "Title is required", type: "error" });
+      return;
+    }
+
+    try {
+      const response = await createNewTodo({ title, description });
+      if (response) {
+        setToast({ message: " Todo added successfully", type: "success" });
+        console.log("Todo added successfully", response.data);
+
+        setTitle("");
+        setDescription("");
+      }
+    } catch (error) {
+      setToast({ message: " Something went wrong", type: "error" });
+      console.log(error);
     }
   };
 
@@ -30,6 +58,17 @@ function App() {
         />
         <Button label="Add" onClick={createTodo} className="px-10" />
       </div>
+      <div className="flex flex-col p-4 ">
+        {todo.map((todo) => {
+          <h1>{todo.title}</h1>;
+          <p>{todo.description}</p>;
+        })}
+      </div>
+      <Toast
+        message={toast.message}
+        type={toast.type}
+        onClose={() => setToast({ message: "", type: "" })}
+      />
     </div>
   );
 }
