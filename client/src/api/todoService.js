ee92faa87aba218c95ecca7e2a131543ec9d8116
Mohammed Farshad <imohammedfarshad@gmail.com>
2024-11-23 02:12:25 +0530

    remove old client and server

--- client/src/api/todoService.js
+++
@@ -1,8 +0,0 @@
-import axiosClient from "./axiosClient";
-
-const createNewTodo = (data) => axiosClient.post("/todos", data);
-const updateTodoById = (id, title) => axiosClient.put(`/todos/${id}`, title);
-const deleteTodoById = (id) => axiosClient.delete(`/todos/${id}`);
-const getTodos = () => axiosClient.get("/todos");
-
-export { createNewTodo, updateTodoById, deleteTodoById, getTodos };
--- server/src/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { registerUser } from "../controllers/authController.js";
-
-const router = Router();
-
-router.route("/register").post(registerUser);
-
-export default router;
