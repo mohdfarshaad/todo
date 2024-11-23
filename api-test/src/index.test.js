101daea65fbd476cea4513178af5f1b76667fde8
Mohammed Farshad <imohammedfarshad@gmail.com>
2024-11-24 02:03:37 +0530

    Add all test for signup route

--- api-test/src/index.test.js
+++ api-test/src/index.test.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const BASE_URL = "http://localhost:3000/api/v1";
 
 describe("Sign up ", () => {
-  const email = `user${Math.floor(Math.random() * 10 + 1)}@gmail.com`;
+  const email = `user${Math.floor(Math.random() * 10) + 10}@gmail.com`;
   const password = "1234";
 
   test("With correct credintials", async () => {
@@ -12,13 +12,42 @@ describe("Sign up ", () => {
         email,
         password,
       });
-
-      console.log(response.data);
-
       expect(response.status).toBe(201);
     } catch (error) {
-      console.log("error : ", error);
       throw error;
+    }
+  });
+
+  test("With missing email", async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}/users/signup`, {
+        password,
+      });
+    } catch (error) {
+      expect(error.response.status).toBe(400);
+    }
+  });
+
+  test("With missing password", async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}/users/signup`, {
+        email,
+      });
+    } catch (error) {
+      expect(error.response.status).toBe(400);
+    }
+  });
+
+  test("With Existing credintials", async () => {
+    const eEmail = "user18@gmail.com"; //already existing email
+
+    try {
+      const response = await axios.post(`${BASE_URL}/users/signup`, {
+        email: eEmail,
+        password,
+      });
+    } catch (error) {
+      expect(error.response.status).toBe(409);
     }
   });
 });
